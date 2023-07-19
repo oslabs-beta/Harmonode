d40cbd72e7cb35edad5cbae31fe5a3e7bc17e822
Sebastian Sarmiento <sebastiansarm@gmail.com>
2023-07-19 12:07:41 -0400

    made change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,18 +6,37 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import List from './pages/List/List';
 import Diagram from './pages/Diagram/Diagram';
 import Settings from './pages/Settings/Settings';
+import { DirTreeHolder } from './contextStore/contextStore';
+
+const initialState = { dirTree: {} };
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "setDirTree" : 
+      return {...state, dirTree: action.payload} 
+    default :
+      return {...state}
+  }
+}
 
 const App = () => {
+  // const [someVar, setSomeVar] = useState(7);
+  // const [globalNames, namesDispatcher] = useReducer(reducer, initialState)
+
   const navigate = useNavigate();
 
+// const reducer = (state, { type, payroll })
   function navClick(path) {
     navigate(path);
   }
 
   return (
     <div className='app'>
+
+    {/* <NamesHolder.Provider value={{globalNames}}> */}
       <Sidebar />
-      <div className='content'>
+      {/* </NamesHolder.Provider> */}
+
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/projects' element={<ProjectsPage />} />
@@ -25,7 +44,7 @@ const App = () => {
           <Route path='/diagram' element={<Diagram />} />
           <Route path='/settings' element={<Settings />} />
         </Routes>
-      </div>
+
     </div>
   );
 };
