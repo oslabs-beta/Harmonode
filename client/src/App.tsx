dc07fe2ef261abe487a06834054038b463c03fc8
hmz44 <126123010+hmz44@users.noreply.github.com>
2023-07-20 17:12:16 -0400

    react-flow-tweaks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -35,7 +35,7 @@ const App = () => {
 
     {/* <NamesHolder.Provider value={{globalNames}}> */}
       <Sidebar />
-      <Diagram />
+      
       {/* </NamesHolder.Provider> */}
 
         <Routes>
--- client/src/pages/Diagram/Diagram.tsx
+++ client/src/pages/Diagram/Diagram.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import ReactFlow, { Edge } from 'reactflow';
+import React, { useCallback, useState } from 'react';
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  MiniMap,
+  Background,
+  BackgroundVariant,
+  Controls,
+} from 'reactflow';
 import 'reactflow/dist/style.css';
 
 function Diagram() {
@@ -10,11 +18,31 @@ function Diagram() {
 
   const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges])
+
+  const onNodeClick = useCallback((event, node) => {
+    console.log(node, 'node clicked')
+  },[])
+
   return (
-    // must wrap <ReactFlow /> with a width and height
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: '100vw', height: '100vh', backgroundColor: '#526D82' }}>
       Diagram
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onNodeClick={onNodeClick} // to test button
+      >
+        <Controls />
+        <MiniMap zoomable pannable />
+        <Background id="1" gap={10} color="#f1f1f1" variant={BackgroundVariant.Lines} />
+        <Background id="2" gap={100} offset={1} color="#ccc" variant={BackgroundVariant.Lines} />
+      </ReactFlow>
     </div>
   );
 }
