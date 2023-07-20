623e5aa638a2d7ae254b16e7f6466122e086f872
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-20 12:50:36 -0400

    using fs.watch now! Much better

--- server/utils/monitorFileChanges.ts
+++ server/utils/monitorFileChanges.ts
@@ -3,64 +3,50 @@ import * as fs from 'fs';
 // Will need to be on a setTimeout, cleared only when another project is loaded
 // factory function for monitoring the files
 export default function monitorFiles(astRootObj) {
-  // setting a monitor loop function -- may be unnecessary, but keeping for now
-  function monitorLoop() {
-    // grab all of the files from the astRootObj
-    const fetchFiles = astRootObj.fetchFiles;
-    const fetches = astRootObj.fetches;
-    const endpoints = astRootObj.endpoints;
-    const endpointFiles = astRootObj.endpointFiles;
+  const fetchFiles = astRootObj.fetchFiles;
+  const fetches = astRootObj.fetches;
+  const endpoints = astRootObj.endpoints;
+  const endpointFiles = astRootObj.endpointFiles;
 
-    // hardcoded 1second delay - which is the frequency for the monitor loop
-    setTimeout(async () => {
-      // if there's a change in any of these files, it will trigger the conditions below
-      const fetchFileChange = await checkFetchFiles(fetchFiles);
-      const fetchChange = await checkFetches(fetches);
-      const endpointFileChange = await checkEndpointFiles(endpointFiles);
-      const endpointChange = await checkEndpoints(endpoints);
-
-      // what we should do if there's a change detected
-      if (fetchFileChange) {
-        console.log(`Change in fetchFiles at ${fetchFileChange} detected`);
-      }
-      if (fetchChange) {
-        console.log(`Change in fetch at ${fetchChange} detected`);
-      }
-      if (endpointFileChange) {
-        console.log(
-          `Change in endpointFiles at ${endpointFileChange} detected`
-        );
-      }
-      if (endpointChange) {
-        console.log(`Change in endpoints at ${endpointChange} detected`);
-      }
-      monitorLoop();
-    }, 1000);
-  }
-  monitorLoop();
-}
-
-async function checkFetchFiles(fetchFiles) {
+  /*
+  =====================
+  FetchFiles Monitoring
+  =====================
+  */
   for (const file of fetchFiles) {
-    const filePath: string = file.fullPath;
-    const lastUpdated: Date = file.lastUpdated;
-    const fsStats: fs.Stats = fs.statSync(filePath);
+    // some editors sometimes trigger two change events, so we can do this based on modified time
+    fs.watch(file.fullPath, (eventType, filename) => {
+      let fsStats: fs.Stats;
 
-    const mTime = new Date(fsStats.mtime);
+      // ===File Rename Event===
+      if (eventType === 'rename') {
+        // update the file name
+        console.log(`${file.fileName} was renamed to ${filename}`);
+        file.fullPath = `${file.filePath}/${filename}`;
+        file.fileName = filename;
+      }
 
-    if (mTime.getTime() != lastUpdated.getTime()) {
-      file.lastUpdated = mTime;
-      return file.fileName;
-    }
+      // ===File Change Event===
+
+      // renaming triggers a change event first, which causes an error -
+      // because the filename on the change is the previous one - so it doesn't exist
+      // this try catch will just return since we don't care about that change
+      try {
+        fsStats = fs.statSync(file.fullPath);
+      } catch (error) {
+        return;
+      }
+      // get the last modified time
+      const mTime = fsStats.mtime;
+
+      // we have to do this check to avoid two change events firing
+      // which VSCode and other code editors emit sometimes
+
+      // if the last modified time is different than what we saved, do stuff...
+      if (mTime.getTime() != file.lastUpdated.getTime()) {
+        file.lastUpdated = mTime; // update the last modified time
+        console.log(`The file ${filename} was ${eventType}d`);
+      }
+    });
   }
-  return null;
-}
-async function checkFetches(fetches) {
-  return null;
-}
-async function checkEndpointFiles(endpointFiles) {
-  return null;
-}
-async function checkEndpoints(endpoints) {
-  return null;
 }
