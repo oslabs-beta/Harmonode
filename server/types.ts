2610f4dd41f35d4a0d6eb3555fe2e6d65958cf2e
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-19 20:02:57 -0400

    sudo code for the types

--- server/types.ts
+++ server/types.ts
@@ -1,16 +1,19 @@
+// file object for information about files read with fs
 export interface FileObj {
   fileName: string;
   filePath: string;
   fullPath: string;
   contents: string;
-  mDate?: Date;
+  mDate?: Date; // <--- last modified date, used for knowing when ast will update the file
 }
 
+// directory object for information about directories read with fs
 export interface DirObj {
   fileName: string;
   filePath: string;
 }
 
+// the root ast structure which has all the ast data collected in it
 export interface astRoot {
   fetches: astFetch[];
   endPoints: astEndpoint[];
@@ -18,6 +21,7 @@ export interface astRoot {
   endpointFiles: astEndpointFile[];
 }
 
+// produces information from ast parser with information on a per file basis for fetch requests
 export interface astFetchFile {
   fileName: string;
   fullPath: string;
@@ -25,8 +29,11 @@ export interface astFetchFile {
   fetches: string[];
 }
 
+// produces information from ast parser with information on a per file basis for endpoint details
 export interface astEndpointFile {}
 
+// produces information derived from ast parser with information on a per fetch request basis
 export interface astFetch {}
 
+// produces information derived from ast parser with information on a per endpoint basis
 export interface astEndpoint {}
