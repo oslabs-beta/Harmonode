aaa11b38e5f98d72ca34523020cb0afc94196302
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-20 23:26:25 -0400

    start of project list item creation

--- client/src/pages/Projects/components/ListProjects.tsx
+++ client/src/pages/Projects/components/ListProjects.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router';
 import {getProjects} from '../../../ipcRenderer';
+import ProjectListCard from './ProjectListCard';
 
 interface projectObject {
   name: string;
@@ -16,11 +17,7 @@ function ListProjects() {
   const navigate = useNavigate();
 
   const projectComponents = projects.map((project) => {
-    return (
-      <>
-        <p>{project.name}: </p> <span>{project.folder}</span>
-      </>
-    );
+    return <ProjectListCard project={project} />;
   });
 
   useEffect(() => {
@@ -31,21 +28,15 @@ function ListProjects() {
     loadProjects();
   }, []);
 
-  // function addProject() {
-  //   const newProjects = [...projects]; // 0x000123 -> new Array [] -> new address 0x000124
-  //   newProjects.push('New Project'); // newProjects = 0x000123 ...
-  //   console.log(projects);
-  //   setProjects(newProjects);
-  // }
-
   function navigateElsewhere() {
     navigate('/diagram');
   }
 
   return (
     <div>
-      <h1>Projects :</h1> {projectComponents}
-      <button onClick={navigateElsewhere}>Navigate Elsewhere</button>
+      <h1>Projects :</h1>
+      {projectComponents}
+      <button onClick={navigateElsewhere}>Clear Project List</button>
     </div>
   );
 }
