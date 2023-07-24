f98d4cdb00c41dd13796904188547deb620f722f
hmz44 <126123010+hmz44@users.noreply.github.com>
2023-07-24 19:36:51 -0400

    reactflow-map

--- client/src/pages/Diagram/Diagram.tsx
+++ client/src/pages/Diagram/Diagram.tsx
@@ -1,7 +1,5 @@
-import React, {useCallback, useState, useContext, useEffect} from 'react';
-import AddProject from '../Projects/components/AddProject';
-import ProjectsProvider from '../../context/ProjectsProvider';
-import {ProjectsContext} from '../../context/contextStore';
+import React, { useCallback, useState, useContext, useEffect } from 'react';
+import { ProjectsContext } from '../../context/contextStore';
 import ReactFlow, {
   useNodesState,
   useEdgesState,
@@ -10,15 +8,10 @@ import ReactFlow, {
   Background,
   BackgroundVariant,
   Controls,
+  Position,
+  useNodes,
 } from 'reactflow';
-import 'reactflow/dist/style.css';
-import {PlaylistAddOutlined} from '@mui/icons-material';
-// start with fetchFiles?
-// get data from interface astroot in types.ts
-// formSubmit in addproject.tsx
-// setActiveProject holds all data thats loaded for current project
-// ast:files
-// grab name from active project name and state
+// import 'reactflow/dist/styles.css/';
 
 // Need some function to be able to create nodes each time "save and load project" is clicked
 // Will also render the data being passed onto the nodes
@@ -31,25 +24,48 @@ function Diagram() {
   // onclick of "save project and load" button nodes are created
   // show all files EXCEPT the ones that were selected to be ignored
 
-  const {activeProject} = useContext(ProjectsContext);
+  const { activeProject } = useContext(ProjectsContext);
 
-  // console.log(activeProject);
+  console.log(activeProject);
 
   const nodeColor = (node) => {
-    switch (node.id) {
+    switch (node.type) {
+      case 'filenameNode':
+        return '#98DFD6';
+      case 'endpointNode':
+        return '#19A7CE';
       case '1':
         return '#FCC8D1';
-      case '2':
-        return '#98DFD6';
       case '3':
         return '#FFF89C';
-      case '4':
-        return '#19A7CE';
       default:
         return '#ff0072';
     }
   };
 
+  // const filenameNode = {
+  //   border: '3px solid #1B9C85',
+  //   background: '#98DFD6',
+  //   borderRadius: 15,
+  // };
+
+  // const endpointNode = {
+  //   border: '3px solid #146C94',
+  //   background: '#19A7CE',
+  //   borderRadius: 15,
+  // };
+
+  // const defaultNodeStyle1 = {
+  //   border: '3px solid #ff0071',
+  //   background: '#FCC8D1',
+  //   borderRadius: 15,
+  // };
+
+  // const defaultNodeStyle3 = {
+  //   border: '3px solid #FFEA11',
+  //   background: '#FFF89C',
+  //   borderRadius: 15,
+  // };
   const defaultNodeStyle1 = {
     border: '3px solid #ff0071',
     background: '#FCC8D1',
@@ -88,36 +104,61 @@ function Diagram() {
       style: defaultNodeStyle1
     }
   }) */
-
-  //if (activeProject.hasOwnProperty('ast')
-  // if not set initialnode = []
-  // if it does set it to map below
-  const initialNodes = activeProject.ast.fetchFiles.map((file, idx) => {
+  const initialFetchNodes = activeProject.ast.fetchFiles.map((file, idx) => {
     return {
-      id: idx.toString(),
+      id: file.id,
       position: {x: 0, y: idx * 100},
       data: {label: file.fileName}, //each file needs an id and we'll use the id to connect the nodes
       style: defaultNodeStyle1,
     };
   });
 
+  const initialEndpointNodes = activeProject.ast.endpointFiles[0].endpoints.map(
+    (file, idx) => {
+      return {
+        id: idx.toString(),
+        position: {x: 200, y: idx * 100},
+        data: {label: file},
+        style: defaultNodeStyle2,
+      };
+    }
+  );
+
+  const initialNodes = [...initialFetchNodes, ...initialEndpointNodes];
+  console.log(initialNodes, 'INITIAL NODES');
   // Need to add functionality so that for each proj. load..
   // it will create nodes based on what is necesscary
   // We determine how many nodes are necesscary based on what user selected and on fileLoad for count?
   const initialEdges = [
-    { id: 'e0-1', source: '0', target: '1' }, //source MUST match id in order to connect - filename will be ID
-    { id: 'e1-2', source: '1', target: '2' },
-    { id: 'e2-3', source: '2', target: '3' },
-    { id: 'e3-4', source: '3', target: '4' },
+    {id: 'e0-1', source: '0', target: '1'}, //source MUST match id in order to connect
+    {id: 'e1-2', source: '1', target: '2'},
+    {id: 'e2-3', source: '2', target: '3'},
+    {id: 'e3-4', source: '3', target: '4'},
   ];
 
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialFetchNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  // const nodesInitialized = useNodesInitialized();
 
   useEffect(() => {
-    setNodes(initialNodes);
-    setEdges(initialEdges);
+    // const newNodes = nodes.map((node) => {
+    //   const match = activeProject.ast.fetchFiles.find(
+    //     (file) => file.id === node.id
+    //   );
+    //   if (match) {
+    //     return {
+    //       ...node,
+    //       data: {label: match.fileName},
+    //     };
+    //   }
+    // });
+    // setNodes(newNodes as any);
+    // setEdges(initialEdges);
   }, [activeProject]);
+
+  // useEffect(() => {
+  //   fitView();
+  // }, [nodesInitialized]);
   // combine nodes with spread?
 
   // const connectNodes () => {
@@ -135,8 +176,14 @@ function Diagram() {
   }, []);
 
   return (
-    <div style={{width: '100vw', height: '100vh', backgroundColor: '#526D82'}}>
-      Diagram
+    <div
+      style={{
+        width: '100vw',
+        height: 'calc(100vh - 3em)',
+        backgroundColor: '#526D82',
+        overflow: 'hidden',
+      }}
+    >
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -144,6 +191,7 @@ function Diagram() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         onNodeClick={onNodeClick} // to test if node is clicked
+        proOptions={{hideAttribution: true}}
       >
         <Controls />
         <MiniMap nodeColor={nodeColor} zoomable pannable />
