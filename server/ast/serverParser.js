4fd2f5c242d17134154187bd3214572291b4eed3
kfiddle <kenjfiddle@gmail.com>
2023-07-24 22:26:26 -0400

    serverParser changes and createComponent changes

--- server/ast/serverParser.js
+++ server/ast/serverParser.js
@@ -7,7 +7,30 @@ const endpointParse = (codeString) => {
     plugins: ["jsx"],
   });
 
-  const serverEndPoints = [];
+  const routesObj = {serverEndPoints: []};
+  const importedRoutes = {};
+
+  trav(ast, {
+    enter(path) {
+      if (path.node.type === "VariableDeclarator") {
+        if (
+          path.node.init.type === "CallExpression" &&
+          path.node.init.callee.name === "require"
+        ) {
+          importedRoutes[path.node.id.name] = path.node.init.arguments[0].value;
+        }
+      }
+    },
+  });
+
+  const findOriginalVal = (variable) => {
+    if (
+      variable in importedRoutes &&
+      typeof importedRoutes[variable] === "string"
+    )
+      return importedRoutes[variable];
+    return "deadEnd";
+  };
 
   trav(ast, {
     enter(path) {
@@ -18,13 +41,22 @@ const endpointParse = (codeString) => {
         current.callee.object.name === "app" &&
         current.callee.property.name === "use"
       ) {
-        if (current.arguments[0].value)
-          serverEndPoints.push(current.arguments[0].value);
+        if (current.arguments[0].value) {
+          routesObj.serverEndPoints.push(current.arguments[0].value);
+          if (
+            current.arguments[1] &&
+            current.arguments[1].type === "Identifier"
+          ) {
+            const nextInLine = current.arguments[1];
+            routesObj.serverEndPoints.push(findOriginalVal(current.arguments[1].name));
+            routesObj[nextInLine.name] = findOriginalVal(nextInLine.name);
+          }
+        }
       }
     },
   });
 
-  return serverEndPoints;
+  return routesObj;
 };
 
 export default endpointParse;
--- server/utils/createComponentObject.ts
+++ server/utils/createComponentObject.ts
@@ -1,13 +1,15 @@
-import fetchParser from '../ast/clientParser';
-import endpointParse from '../ast/serverParser';
+import * as path from "path";
+
+import fetchParser from "../ast/clientParser";
+import endpointParse from "../ast/serverParser";
 import {
   astEndpoint,
   astEndpointFile,
   astFetch,
   astFetchFile,
   astRoot,
-} from '../types';
-import {v4 as uuid} from 'uuid';
+} from "../types";
+import { v4 as uuid } from "uuid";
 
 // module that creates the component object that will be sent to the front end
 
@@ -29,11 +31,18 @@ export default function createComponentObject(codeFiles, serverPath) {
 
 function pushFilesToCompObj(codeFiles, componentObj, serverPath) {
   const fetchPaths = {};
+  const filePaths = [];
   for (const file of codeFiles) {
+    const pathParts: string[] = file.fullpath.split(path.sep);
+    let last: string = pathParts.at(-1);
+    if (last.includes("."))
+      pathParts[pathParts.length - 1] = last.slice(0, last.indexOf("."));
+    filePaths.push(pathParts);
     // if it's the server path, let's load the server stuff into an ast
     if (file.fullPath === serverPath) {
       // get the AST for the server
-      const parsedEndpointsArray = endpointParse(file.contents);
+      const serverObj = endpointParse(file.contents);
+      const parsedEndpointsArray = serverObj.serverEndPoints;
       if (parsedEndpointsArray.length > 0) {
         componentObj.endpointFiles.push({
           fileName: file.fileName,
@@ -44,7 +53,11 @@ function pushFilesToCompObj(codeFiles, componentObj, serverPath) {
           isServer: true,
           endpoints: parsedEndpointsArray,
         });
+
+        componentObj.fromImports = serverObj;
+        componentObj.filePaths = filePaths;
       }
+
       continue; // skip the rest since we have what we need
     }
     // getting the AST for fetches
@@ -86,14 +99,14 @@ function isLocalHost(url) {
 }
 
 function getEndpoint(url) {
-  if (typeof url !== 'string') return 'unknownurl';
+  if (typeof url !== "string") return "unknownurl";
   // Check if the URL starts with 'http' or '/' to determine if it's a non-local URL or just a path
-  if (!url.startsWith('http') && !url.startsWith('/')) {
-    if (isLocalHost(url.split('/')[0])) {
+  if (!url.startsWith("http") && !url.startsWith("/")) {
+    if (isLocalHost(url.split("/")[0])) {
       // It's a local URL without the protocol
       // Extract the endpoint by removing the domain and protocol from the URL
-      const urlParts = url.split('/');
-      return `/${urlParts.slice(1).join('/')}`;
+      const urlParts = url.split("/");
+      return `/${urlParts.slice(1).join("/")}`;
     }
     // It's just a path, return it as is
     return url;
@@ -102,8 +115,8 @@ function getEndpoint(url) {
   // Check if the URL is a local URL
   if (isLocalHost(new URL(url).hostname)) {
     // Extract the endpoint by removing the domain and protocol from the URL
-    const urlParts = url.split('/');
-    return `/${urlParts.slice(3).join('/')}`;
+    const urlParts = url.split("/");
+    return `/${urlParts.slice(3).join("/")}`;
   }
 
   // It's a non-local URL, return it as is
