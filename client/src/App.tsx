66076ae68e9031a248b2f878c9f16863c88ee365
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-26 12:15:18 -0400

    hotfix on diagram

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,14 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import {Route, Routes, useNavigate} from 'react-router';
 import ProjectsPage from './pages/Projects/ProjectsPage';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard/Dashboard';
 import List from './pages/List/List';
-import Diagram from './pages/Diagram/components/Diagram';
 import Settings from './pages/Settings/Settings';
-import {DirTreeHolder} from './context/contextStore';
-import ProjectsProvider from './context/ProjectsProvider';
 import Topbar from './components/Topbar';
-
-//establishes initial state
-const initialState = {dirTree: {name: 'tree'}};
-
-//update and return new state
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'setDirTree':
-      return {...state, dirTree: {name: action.payload}};
-    default:
-      return {...state};
-  }
-};
+import DiagramPage from './pages/Diagram/DiagramPage';
 
 const App = () => {
-  const [globalDir, dirDispatcher] = useReducer(reducer, initialState);
-
   const navigate = useNavigate();
 
   function navClick(path) {
@@ -43,7 +26,7 @@ const App = () => {
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/projects' element={<ProjectsPage />} />
           <Route path='/list' element={<List />} />
-          <Route path='/diagram' element={<Diagram />} />
+          <Route path='/diagram' element={<DiagramPage />} />
           <Route path='/settings' element={<Settings />} />
         </Routes>
       </div>
