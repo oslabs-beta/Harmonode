954cfa51b3409d7b05400a1e7d33e84a93630835
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-26 21:12:35 -0400

    diagram styled to include methods on fetch file requests

---
+++ client/src/pages/Diagram/components/PathNode.tsx
@@ -0,0 +1,68 @@
+import React, {useCallback} from 'react';
+import {Handle, Position} from 'reactflow';
+
+const handleStyle = {left: 10};
+const handleStyle2 = {left: 35};
+const handleStyle3 = {left: 60};
+const handleStyle4 = {left: 85};
+const handleStyle5 = {left: 110};
+
+function PathNode({data, isConnectable}) {
+  return (
+    <div className='text-updater-node'>
+      <Handle
+        type='target'
+        position={Position.Top}
+        isConnectable={isConnectable}
+      />
+      <div
+        style={{
+          height: '2.125rem',
+          width: '9em',
+          justifyContent: 'center',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        {data.label}
+      </div>
+      <Handle
+        type='source'
+        position={Position.Bottom}
+        id='a'
+        style={handleStyle}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type='source'
+        position={Position.Bottom}
+        id='b'
+        style={handleStyle2}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type='source'
+        position={Position.Bottom}
+        id='c'
+        style={handleStyle3}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type='source'
+        position={Position.Bottom}
+        id='d'
+        style={handleStyle4}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type='source'
+        position={Position.Bottom}
+        id='e'
+        style={handleStyle5}
+        isConnectable={isConnectable}
+      />
+    </div>
+  );
+}
+
+export default PathNode;
