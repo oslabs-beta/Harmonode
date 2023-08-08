01ea04851e721ef0c2427345119580fa7a552963
EricDalio <ericdalio@gmail.com>
2023-08-07 22:26:25 -0400

    theme settings

--- client/src/components/Topbar.tsx
+++ client/src/components/Topbar.tsx
@@ -1,17 +1,17 @@
-import React, {useContext} from 'react';
-import {ProjectsContext} from '../context/contextStore';
+import React, { useContext } from 'react';
+import { ProjectsContext } from '../context/contextStore';
 
 function Topbar() {
-  const {activeProject} = useContext(ProjectsContext);
+  const { activeProject } = useContext(ProjectsContext);
   console.log(activeProject);
   return (
     <div className={`topbar ${!activeProject.name && 'topbar-inactive'}`}>
       {activeProject.name ? (
-        <p style={{color: 'var(--font-main-color)'}}>
+        <p style={{ color: 'var(--font-main-color)' }}>
           Active Project: <b>{activeProject.name}</b> ({activeProject.folder})
         </p>
       ) : (
-        <p style={{color: 'var(--font-main-color)'}}>No Active Project </p>
+        <p style={{ color: 'var(--font-main-color)' }}>No Active Project </p>
       )}
     </div>
   );
--- client/src/pages/Settings/components/Settings.tsx
+++ client/src/pages/Settings/components/Settings.tsx
@@ -1,6 +1,9 @@
+import { flexbox } from '@mui/system';
 import React, { useState } from 'react';
 import { SketchPicker } from 'react-color';
 import ColorPicker from 'react-color-picker';
+import { HexColorPicker } from 'react-colorful';
+import './settings.css';
 
 function Settings() {
   const defaultTheme = {
@@ -46,22 +49,60 @@ function Settings() {
     '--inactive-color': '#FAD6A5',
     '--font-main-color': '#A7EDE7',
   };
-  //set Choose Your Own Adventure state
+  //set customize theme button state
   const [themeChoice, setChoice] = useState(false);
 
-  //set color picker state
-  const primaryColor = '--primary-color';
-  const [primaryColorChoice, setColorChoice] = useState({
-    '--primary-color': primaryColor,
-  });
+  //set default color picker state
+  const [primaryColorChoice, setPimaryColorChoice] = useState('#164b60');
+  const [secondaryColorChoice, setSecondaryColorChoice] = useState('#1b6b93');
+  const [tertiaryColorChoice, setTertiaryColorChoice] = useState('#42A186');
+  const [quatenaryColorChoice, setQuatenaryColorChoice] = useState('#04D6B2');
+  const [inactiveColorChoice, setInactiveColorChoice] = useState('#868484');
+  const [fontManColorChoice, setFontMaiColorChoice] = useState('#f6fff5');
 
-  function handleChangeComplete(color) {
-    setColorChoice({ '--primary-color': color.hex });
+  //color pickker - handle primary color
+  function handlePrimaryColor(color) {
+    document.documentElement.style.setProperty('--primary-color', color);
   }
-  function onDrag(color) {
-    setColorChoice({ '--primary-color': color.hex });
+  //color pickker - handle secondary color
+  function handleSecondaryColor(color) {
+    document.documentElement.style.setProperty('--secondary-color', color);
+  }
+  //color pickker - handle tertiary color
+  function handleTertiaryColor(color) {
+    console.log('color ', color);
+    document.documentElement.style.setProperty('--tertiary-color', color);
+  }
+  //color pickker - handle quaternary color
+  function handleQuaternaryColor(color) {
+    document.documentElement.style.setProperty('--quaternary-color', color);
+  }
+  //color pickker - handle inactive color
+  function handleInactiveColor(color) {
+    document.documentElement.style.setProperty('--inactive-color', color);
+  }
+  //color pickker - handle font-main color
+  function handleFontMainColor(color) {
+    document.documentElement.style.setProperty('--font-main-color', color);
   }
 
+  //handle theme botton choices
+  document.documentElement.style.setProperty('--secondary-color', '#1b6b93');
+  const [showThemeChoices, setThemeButtonStatus] = useState(false);
+  function themeChoiceClick(e) {
+    console.log('CLICK');
+    console.log('showThemeChoices1 ', showThemeChoices);
+    if (!showThemeChoices) {
+      console.log('Change to true');
+      setThemeButtonStatus(true);
+    } else {
+      setThemeButtonStatus(false);
+      console.log('Change to false');
+    }
+    console.log('showThemeChoices2 ', showThemeChoices);
+  }
+
+  //handle color theme selection
   function handleClick(theme, e) {
     if (theme !== 'themeChoose') {
       for (let key in theme) {
@@ -76,71 +117,100 @@ function Settings() {
     }
   }
 
-  function handleSubmit(e, variable) {
-    e.preventDefault();
-    console.log('e.target.inputField.value: ', e.target.inputField.value);
-    document.documentElement.style.setProperty(
-      variable,
-      e.target.inputField.value
-    );
-  }
-
   return (
     <div>
       <h1>Settings</h1>
-      <h2>Themes:</h2>
-      <button onClick={(e) => handleClick(defaultTheme, e)}>
-        Blue Mode (Default)
+      <button
+        className={
+          !showThemeChoices ? 'primaryButtonInactive' : 'primaryButtonActive'
+        }
+        onClick={themeChoiceClick}
+      >
+        {!showThemeChoices ? 'Change Themes' : 'Hide Change Themes'}
       </button>
-      <button onClick={(e) => handleClick(themeOption1, e)}>Dark Mode</button>
-      <button onClick={(e) => handleClick(themeOption2, e)}>Beach Mode</button>
-      <button onClick={(e) => handleClick(themeOption3, e)}>Purple Mode</button>
-      <button onClick={(e) => handleClick(themeOption4, e)}>Forest Mode</button>
-      <button onClick={(e) => handleClick('themeChoose', e)}>
-        Choose Your Own Adventure
-      </button>
-      {themeChoice ? (
+      {showThemeChoices ? (
         <section>
-          <ColorPicker value={primaryColor} onDrag={onDrag} />
-          <SketchPicker
-            color={primaryColorChoice}
-            onChangeComplete={handleChangeComplete}
-          />
-          <form
-            onSubmit={(e) => handleSubmit(e, '--primary-color')}
-            style={{ display: 'flex' }}
+          <button
+            className='button'
+            onClick={(e) => handleClick(defaultTheme, e)}
           >
-            <input name='inputField' placeholder='type hex code' />
-            <button>Primary Color</button>
-          </form>
-          <form
-            onSubmit={(e) => handleSubmit(e, '--secondary-color')}
-            style={{ display: 'flex' }}
+            Blue Mode (Default)
+          </button>
+          <button
+            className='button'
+            onClick={(e) => handleClick(themeOption1, e)}
           >
-            <input name='inputField' placeholder='type hex code' />
-            <button>Secondary Color</button>
-          </form>
-          <form
-            onSubmit={(e) => handleSubmit(e, '--tertiary-color')}
-            style={{ display: 'flex' }}
+            Dark Mode
+          </button>
+          <button
+            className='button'
+            onClick={(e) => handleClick(themeOption2, e)}
           >
-            <input name='inputField' placeholder='type hex code' />
-            <button>Tertiary Color</button>
-          </form>
-          <form
-            onSubmit={(e) => handleSubmit(e, '--quaternary-color')}
-            style={{ display: 'flex' }}
+            Beach Mode
+          </button>
+          <button
+            className='button'
+            onClick={(e) => handleClick(themeOption3, e)}
           >
-            <input name='inputField' placeholder='type hex code' />
-            <button>Quaternary Color</button>
-          </form>
-          <form
-            onSubmit={(e) => handleSubmit(e, '--font-main-color')}
-            style={{ display: 'flex' }}
+            Purple Mode
+          </button>
+          <button
+            className='button'
+            onClick={(e) => handleClick(themeOption4, e)}
           >
-            <input name='inputField' placeholder='type hex code' />
-            <button>Font Color</button>
-          </form>
+            Forest Mode
+          </button>
+
+          <section>
+            <button
+              className={
+                !themeChoice ? 'primaryButtonInactive' : 'primaryButtonActive'
+              }
+              onClick={(e) => handleClick('themeChoose', e)}
+            >
+              {' '}
+              {themeChoice ? 'Hide Customize Colors' : 'Customize Colors'}
+            </button>
+          </section>
+          <section>
+            {themeChoice ? (
+              <section className='container'>
+                <p>Primary Color</p>
+                <p>Secondary Color</p>
+                <p>Tertiary Color</p>
+                <p>Quatenary Color</p>
+                <p>Inactive Color</p>
+                <p>Main Font Color</p>
+
+                <HexColorPicker
+                  color={primaryColorChoice}
+                  onChange={handlePrimaryColor}
+                />
+                <HexColorPicker
+                  color={secondaryColorChoice}
+                  onChange={handleSecondaryColor}
+                />
+                <HexColorPicker
+                  color={tertiaryColorChoice}
+                  onChange={handleTertiaryColor}
+                />
+                <HexColorPicker
+                  color={quatenaryColorChoice}
+                  onChange={handleQuaternaryColor}
+                />
+                <HexColorPicker
+                  color={inactiveColorChoice}
+                  onChange={handleInactiveColor}
+                />
+                <HexColorPicker
+                  color={fontManColorChoice}
+                  onChange={handleFontMainColor}
+                />
+              </section>
+            ) : (
+              <section></section>
+            )}
+          </section>
         </section>
       ) : (
         <section></section>
