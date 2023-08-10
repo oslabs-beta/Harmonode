12fd5580d1ad89d238e3ad508de6607f6388a2b8
kfiddle <kenjfiddle@gmail.com>
2023-08-09 20:37:04 -0400

    forge config change

--- forge.config.js
+++ forge.config.js
@@ -5,22 +5,29 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'win32'],
-      arch: ['arm64', 'x64']
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        authors: 'Harmonode',
+        description: 'Harmonode App'
+      }
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "win32"],
+      arch: ["arm64", "x64"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
